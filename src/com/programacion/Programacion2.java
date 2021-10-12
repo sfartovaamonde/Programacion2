bea31e99cbafae9651930edecb060cffae9197d7
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-10-12 22:50:05 +0200

    Initial commit

---
+++ src/com/programacion/Programacion2.java
@@ -0,0 +1,11 @@
+package com.programacion;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println("Hola programación");
+
+    }
+
+}
