51935d7fbd4847c749c93eeba491ed791c9fd679
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-10-12 23:19:17 +0200

    Ejercicio 1 del Boletin 1

---
+++ src/com/programacion/boletin1/Ejercicio1.java
@@ -0,0 +1,15 @@
+package com.programacion.boletin1;
+
+
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Ej 1 del Boletin 1");
+
+    }
+
+
+
+    }
+
